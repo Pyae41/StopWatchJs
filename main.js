794a3a5f726a0ba78f5e1044ce8269ab9e61c92a
Pyae Phyo Zan <pyae418283@gmail.com>
2022-02-14 11:21:17 +0630

    update whole js

--- main.js
+++ main.js
@@ -2,68 +2,52 @@ const start_stop = document.getElementById("start_stop");
 const reset = document.getElementById("reset");
 const stopwatch = document.getElementById("stopwatch");
 
-let hrs = 0;
-let mins = 0;
-let secs = 0;
-let stopTime = true;
-
+var timeBegan = null,
+    timeStopped = null,
+    stoppedDuration = 0,
+    started = null;
 
 function startTimer() {
-    if (stopTime) {
-        stopTime = false;
-        timeCount();
+    if (timeBegan === null) {
+        timeBegan = new Date();
     }
+
+    if (timeStopped !== null) {
+        stoppedDuration += (new Date() - timeStopped);
+    }
+    console.log(stoppedDuration);
+
+    started = setInterval(clockRunning, 10);
 }
 
 function stopTimer() {
-    if (!stopTime) {
-        stopTime = true
-    }
-}
-
-function timeCount() {
-    if (!stopTime) {
-        secs = parseInt(secs);
-        mins = parseInt(mins);
-        hrs = parseInt(hrs);
-
-        secs = secs + 1;
-
-        if (secs == 60) {
-            mins = mins + 1;
-            secs = 0;
-        }
-        if (mins == 60) {
-            hrs = hrs + 1;
-            mins = 0;
-            secs = 0;
-        }
-
-        if (secs < 10 || secs == 0) {
-            secs = '0' + secs;
-        }
-        if (mins < 10 || mins == 0) {
-            mins = '0' + mins;
-        }
-        if (hrs < 10 || hrs == 0) {
-            hrs = '0' + hrs;
-        }
-
-        stopwatch.innerHTML = hrs + ':' + mins + ':' + secs;
-
-        setTimeout("timeCount()", 1000);
-    }
+    timeStopped = new Date();
+    clearInterval(started);
 }
 
 function resetTimer() {
-    stopwatch.innerHTML = '00:00:00';
-    hrs = 0;
-    mins = 0;
-    secs = 0;
-
-    stopTimer();
-    start_stop.innerHTML = "Start";
+    clearInterval(started);
+    stoppedDuration = 0;
+    timeBegan = null;
+    timeStopped = null;
+    stopwatch.innerHTML = "00:00:00.000";
+    start_stop.innerHTML = "Start"
 }
+
+function clockRunning() {
+    var currentTime = new Date(),
+        timeElapsed = new Date(currentTime - timeBegan - stoppedDuration),
+        hour = timeElapsed.getUTCHours(),
+        min = timeElapsed.getUTCMinutes(),
+        sec = timeElapsed.getUTCSeconds(),
+        ms = timeElapsed.getUTCMilliseconds();
+
+    stopwatch.innerHTML =
+        (hour > 9 ? hour : "0" + hour) + ":" +
+        (min > 9 ? min : "0" + min) + ":" +
+        (sec > 9 ? sec : "0" + sec) + "." +
+        (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+};
 
 start_stop.addEventListener('click', () => {
     if (start_stop.innerHTML == "Start") {
